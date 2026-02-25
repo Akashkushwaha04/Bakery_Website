640ec2c7a8675594492ab51f8c255a1a729ef4c4
Akash kushwaha <kushwaha.akash2005@gmail.com>
2026-02-25 11:33:04 +0530

    Initial commit - Bakery Website

---
+++ script.js
@@ -0,0 +1,7 @@
+const navlist=document.getElementById('navlist');
+const hamburger=document.getElementById('hamburger');
+
+// click event
+hamburger.addEventListener('click',()=>{
+    navlist.classList.toggle('nav-options-active')
+})
